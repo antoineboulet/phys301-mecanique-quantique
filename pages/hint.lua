3a9af0dd078756429f04df342ff3c04026cb4a5b
AntoineBoulet <antoine.boulet@protonmail.com>
2026-06-25 20:38:00 +0200

    update

--- pages/hint.lua
+++
@@ -1,9 +0,0 @@
-function Div(el)
-  if el.classes:includes("indication") then
-    local titre = el.attributes["titre"] or "Indication"
-    local summary = "<summary><em>" .. titre .. "</em></summary>"
-    return pandoc.RawBlock("html", "<details>" .. summary ..
-      pandoc.write(pandoc.Pandoc(el.content), "html") ..
-      "</details>")
-  end
-end
